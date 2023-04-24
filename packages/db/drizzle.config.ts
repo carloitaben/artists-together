f69a12fed229b57f0d70a005c53ff78f270f4653
Carlo <hola.carlodominguez@gmail.com>
2023-04-24 19:07:07 +0200

    one less env var

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -3,5 +3,5 @@ import { type Config } from "drizzle-kit"
 
 export default {
   schema: "./src/schema.ts",
-  connectionString: process.env.DATABASE_URL,
+  connectionString: `mysql://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}/artists-together?ssl={"rejectUnauthorized":true}`,
 } satisfies Config
