90c641c9e48f3da8b87913a5b8fad6951ab47b90
Carlo <hola.carlodominguez@gmail.com>
2023-04-29 19:02:46 +0200

    feat(bot-discord): adds announce script

--- apps/bot-discord/src/lib/env.ts
+++ apps/bot-discord/src/lib/env.ts
@@ -11,8 +11,9 @@ dotenv.config({
 
 const schema = z.object({
   NODE_ENV: z.enum(["development", "production"]).default("development"),
-  DISCORD_BOT_TOKEN: z.string().nonempty(),
+  DISCORD_BOT_ID: z.string().nonempty().optional(),
   DISCORD_SERVER_ID: z.string().nonempty().optional(),
+  DISCORD_BOT_TOKEN: z.string().nonempty(),
 })
 
 export const env = schema.parse(process.env)
