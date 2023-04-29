ed68641b3ecc169f125630b58bcdf259b2c06503
Carlo <hola.carlodominguez@gmail.com>
2023-04-29 18:26:38 +0200

    fix(bot-discord): tries to fix live role

--- apps/bot-discord/src/app/live-role.ts
+++ apps/bot-discord/src/app/live-role.ts
@@ -4,24 +4,22 @@ import { registerEventHandler } from "~/lib/core"
 import { getGuild, getRole } from "~/lib/helpers"
 import { ROLES } from "~/lib/constants"
 
-registerEventHandler("presenceUpdate", async (oldPresence, newPresence) => {
+registerEventHandler("presenceUpdate", async (_, newPresence) => {
   if (!newPresence.guild) return
   if (!newPresence.member) return
   if (!newPresence.user) return
   if (newPresence.user.bot) return
 
-  try {
-    const guild = await getGuild(newPresence.client)
-    const role = await getRole(guild.roles, ROLES.LIVE_NOW)
+  const guild = await getGuild(newPresence.client)
+  const role = await getRole(guild.roles, ROLES.LIVE_NOW)
 
-    const isStreaming = newPresence.activities.some((activity) => {
-      return activity.type === ActivityType.Streaming
-    })
+  const isStreaming = newPresence.activities.some((activity) => {
+    return activity.type === ActivityType.Streaming
+  })
 
-    if (!isStreaming) return newPresence.member.roles.remove(role)
+  if (isStreaming) return newPresence.member.roles.add(role)
 
-    return newPresence.member.roles.add(role)
-  } catch (error) {
-    console.error(error)
+  if (newPresence.member.roles.cache.has(ROLES.LIVE_NOW)) {
+    return newPresence.member.roles.remove(role)
   }
 })
