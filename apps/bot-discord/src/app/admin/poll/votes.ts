e6c6ddc185c3a68ec18e786d36c311f9ec15cea5
Carlo <hola.carlodominguez@gmail.com>
2023-05-03 19:20:23 +0200

    initial poll votes subcommand

--- apps/bot-discord/src/app/admin/poll/votes.ts
+++ apps/bot-discord/src/app/admin/poll/votes.ts
@@ -1,10 +1,28 @@
 import { ChatInputCommandInteraction } from "discord.js"
+import { connect, discordPollVotes, eq } from "db"
+
+import { polls } from "~/store/polls"
 
 export default async function handleVotesPollSubcommand(interaction: ChatInputCommandInteraction) {
-  const name = interaction.options.getString("name", true)
+  const id = parseInt(interaction.options.getString("name", true))
+
+  const poll = polls.get(id)
+  if (!poll) throw Error(`Could not find poll with id ${id}`)
+
+  const db = connect()
+  const votes = await db.select().from(discordPollVotes).where(eq(discordPollVotes.pollId, id))
+
+  console.log(votes)
+
+  if (!votes.length) {
+    return interaction.reply({
+      content: "No one has voted in that poll yet!",
+      ephemeral: true,
+    })
+  }
 
   return interaction.reply({
-    content: `Checking poll stats for poll with id: ${name}`,
+    content: `Found ${votes.length} for poll with id: ${id}`,
     ephemeral: true,
   })
 }
