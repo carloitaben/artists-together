6f255e37c5f13837cc5fa7b9a7171a212f7250a0
Carlo <hola.carlodominguez@gmail.com>
2023-05-14 15:55:24 +0200

    feat: centralizes env files

--- apps/bot-discord/src/lib/env.ts
+++ apps/bot-discord/src/lib/env.ts
@@ -1,13 +1,7 @@
-import * as dotenv from "dotenv"
-import { dirname, join } from "path"
-import { fileURLToPath } from "url"
+import { config } from "dotenv-mono"
 import { z } from "zod"
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
-
-dotenv.config({
-  path: join(__dirname, "../../../../", ".env"),
-})
+config()
 
 const schema = z.object({
   NODE_ENV: z.enum(["development", "production"]).default("development"),
