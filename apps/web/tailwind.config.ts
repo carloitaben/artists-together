d9b8ecdb8c0d08a396b619e5aa5714567e2d7be9
Carlo <hola.carlodominguez@gmail.com>
2023-06-27 12:43:39 +0200

    fonts

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "tailwindcss"
 
 export default {
-  content: ["./app/**/*.{ts,tsx}"],
+  content: ["./src/**/*.{ts,tsx}"],
   theme: {
     fontFamily: {
       sans: "Inter",
