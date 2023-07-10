b9401afa990615670706404201812998b013c84c
Carlo <hola.carlodominguez@gmail.com>
2023-07-10 13:59:32 +0200

    moving things around

--- apps/web/src/app/(routes)/layout.tsx
+++ apps/web/src/app/(routes)/layout.tsx
@@ -7,13 +7,16 @@ import Script from "next/script"
 import "~/styles/index.css"
 
 import { getUser } from "~/services/auth"
+
 import { getTheme, makeThemeStyle, Theme } from "~/lib/themes"
 import { oneOf } from "~/lib/utils"
+
+import { WebSocketProvider } from "~/hooks/ws"
+
 import NavigationSideBar from "~/components/NavigationSideBar"
 import NavigationBottomBar from "~/components/NavigationBottomBar"
 import Toast from "~/components/Toast"
-import { WebSocketProvider } from "~/hooks/ws"
-import Cursors from "~/components/Cursors/Cursors"
+import Cursors from "~/components/Cursors"
 import Cursor from "~/components/Cursor"
 
 type Props = {
@@ -57,9 +60,9 @@ export default async function Layout({ children }: Props) {
             {children}
             <NavigationBottomBar user={user} />
             <Cursors user={user} />
-            <Cursor />
           </WebSocketProvider>
         </Toast>
+        <Cursor />
         <Script id="tailwindcss-noscript" strategy="beforeInteractive">
           {`(function(){typeof document !== "undefined" && document.documentElement.classList.add("js");}())`}
         </Script>
