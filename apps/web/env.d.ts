8e5883d38a0702e4e9c5214d71cd063b894d3fe5
Carlo <hola.carlodominguez@gmail.com>
2023-07-13 13:23:59 +0200

    add client-only package & declares modules

--- apps/web/env.d.ts
+++ apps/web/env.d.ts
@@ -1,5 +1,7 @@
 /// <reference types="lucia" />
 
+declare module "client-only"
+declare module "server-only"
 declare namespace Lucia {
   type Auth = import("./src/services/auth").Auth
   type DatabaseUserAttributes = Omit<import("db").User, "id">
