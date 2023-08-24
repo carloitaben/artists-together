d8736cc260372f1198688db74fe181b2b4b20d3e
Carlo <hola.carlodominguez@gmail.com>
2023-08-24 19:11:29 +0200

    feat: animations

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,5 +1,6 @@
-import Icon from "~/components/Icon"
 import { logo } from "~/components/Icons"
+import Icon from "~/components/Icon"
+import Animations from "./components/Animations"
 
 function Social() {
   const discordHref = "https://discord.gg/9Ayh9dvhHe"
@@ -7,7 +8,7 @@ function Social() {
 
   return (
     <>
-      <h3 className="hidden sm:block">
+      <h3 data-animated-text className="hidden js:invisible sm:block">
         Follow us on{" "}
         <a
           className="underline transition-[font-weight] duration-150 ease-[cubic-bezier(0.4,0,0.2,1)] hover:font-medium"
@@ -29,7 +30,10 @@ function Social() {
         </a>{" "}
         server.
       </h3>
-      <ul className="whitespace-nowrap text-center sm:hidden sm:pb-3">
+      <ul
+        data-animated-text
+        className="whitespace-nowrap text-center js:invisible sm:hidden sm:pb-3"
+      >
         <li className="mx-2.5 inline-block">
           <a
             target="_blank"
@@ -60,13 +64,17 @@ export default function Home() {
     <div className="flex h-full flex-col">
       <header className="-mb-7 self-center px-12 pt-20 sm:mb-0 sm:mt-[3.75rem] sm:self-auto sm:p-0">
         <h1 className="sr-only">Artists Together</h1>
-        <Icon label="" className="w-full max-w-[16.25rem] sm:max-w-[22vw]">
+        <Icon
+          label=""
+          data-element="logo"
+          className="w-full max-w-[16.25rem] js:opacity-0 sm:max-w-[22vw]"
+        >
           {logo}
         </Icon>
       </header>
       <main className="flex flex-1 flex-col justify-end gap-16 overflow-hidden px-3 pb-3 font-serif text-[8.333vw] font-light leading-none xs:text-3xl sm:gap-[4.167vw] sm:pb-[3.25rem] sm:pr-5 sm:text-[5vw]/[5vw]">
         <div className="flex flex-1 items-center justify-center text-center sm:block sm:flex-none sm:text-right">
-          <h2>
+          <h2 data-animated-text className="js:invisible">
             An inclusive community
             <br />
             for all kinds of artists.
@@ -74,6 +82,7 @@ export default function Home() {
         </div>
         <Social />
       </main>
+      <Animations />
     </div>
   )
 }
