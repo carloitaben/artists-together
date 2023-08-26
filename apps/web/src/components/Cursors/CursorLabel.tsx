c487e0d0ac45e8e6e9fc31a55c5fc723e403b1df
Carlo <hola.carlodominguez@gmail.com>
2023-08-26 12:26:05 +0200

    feat(web): modifies emoji label

--- apps/web/src/components/Cursors/CursorLabel.tsx
+++ apps/web/src/components/Cursors/CursorLabel.tsx
@@ -1,15 +1,24 @@
 import { ComponentProps } from "react"
 import { cx } from "class-variance-authority"
 
-type Props = ComponentProps<"div">
+type Props = ComponentProps<"div"> & {
+  emoji?: boolean
+}
 
-export default function CursorLabel({ className, children, ...props }: Props) {
+export default function CursorLabel({
+  emoji,
+  className,
+  children,
+  ...props
+}: Props) {
   return (
     <div
       {...props}
       className={cx(
         className,
-        "inline-flex h-6 items-center justify-center whitespace-nowrap rounded-full bg-not-so-white px-2 text-center text-sm text-not-so-black shadow-[0px_4px_8px_rgba(0,0,0,0.12)]"
+        emoji
+          ? "inline-block text-2xl"
+          : "inline-flex h-6 items-center justify-center whitespace-nowrap rounded-full bg-not-so-white px-2 text-center text-sm text-not-so-black shadow-[0px_4px_8px_rgba(0,0,0,0.12)]",
       )}
     >
       {children}
