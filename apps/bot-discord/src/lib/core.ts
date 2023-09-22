2b4676b631fb40cbd42d09f13bd9a37944b86399
Carlo <hola.carlodominguez@gmail.com>
2023-09-22 18:01:51 +0200

    feat(bot-discord): adds context menu register function

--- apps/bot-discord/src/lib/core.ts
+++ apps/bot-discord/src/lib/core.ts
@@ -2,34 +2,50 @@ import type {
   AutocompleteInteraction,
   ChatInputCommandInteraction,
   ClientEvents,
+  ContextMenuCommandBuilder,
+  MessageContextMenuCommandInteraction,
   SlashCommandBuilder,
+  UserContextMenuCommandInteraction,
 } from "discord.js"
 
 import glob from "fast-glob"
 
-type RegisterEventCallback<T extends keyof ClientEvents> = (...args: ClientEvents[T]) => void
+type RegisterEventCallback<T extends keyof ClientEvents> = (
+  ...args: ClientEvents[T]
+) => void
 
-const handlersMap = new Map<keyof ClientEvents, Set<RegisterEventCallback<any>>>()
+const handlersMap = new Map<
+  keyof ClientEvents,
+  Set<RegisterEventCallback<any>>
+>()
 
-export function registerEventHandler<T extends keyof ClientEvents>(event: T, callback: RegisterEventCallback<T>) {
+export function registerEventHandler<T extends keyof ClientEvents>(
+  event: T,
+  callback: RegisterEventCallback<T>,
+) {
   const set = handlersMap.get(event) ?? new Set<RegisterEventCallback<T>>()
   set.add(callback)
   handlersMap.set(event, set)
 }
 
-type CommandBuilderStub = Pick<SlashCommandBuilder, "name" | "toJSON">
+type CommandBuilderStub = Pick<
+  SlashCommandBuilder | ContextMenuCommandBuilder,
+  "name" | "toJSON"
+>
 
-const slashCommandsMap = new Map<string, CommandBuilderStub>()
+const buildersMap = new Map<string, CommandBuilderStub>()
 
 export function registerSlashCommand<T extends CommandBuilderStub>(
   builder: T,
-  callback: (interaction: AutocompleteInteraction | ChatInputCommandInteraction) => void
+  callback: (
+    interaction: AutocompleteInteraction | ChatInputCommandInteraction,
+  ) => void,
 ) {
-  if (slashCommandsMap.has(builder.name)) {
-    throw Error(`Found duplicated slash command: ${builder.name}`)
+  if (buildersMap.has(builder.name)) {
+    throw Error(`Found duplicated command: ${builder.name}`)
   }
 
-  slashCommandsMap.set(builder.name, builder)
+  buildersMap.set(builder.name, builder)
 
   registerEventHandler("interactionCreate", (interaction) => {
     if (interaction.isAutocomplete()) {
@@ -44,15 +60,39 @@ export function registerSlashCommand<T extends CommandBuilderStub>(
   })
 }
 
+export function registerContextMenuCommand<T extends ContextMenuCommandBuilder>(
+  builder: T,
+  callback: (
+    interaction:
+      | MessageContextMenuCommandInteraction
+      | UserContextMenuCommandInteraction,
+  ) => void,
+) {
+  if (buildersMap.has(builder.name)) {
+    throw Error(`Found duplicated command: ${builder.name}`)
+  }
+
+  buildersMap.set(builder.name, builder)
+
+  registerEventHandler("interactionCreate", (interaction) => {
+    if (interaction.isContextMenuCommand()) {
+      console.log("[contextMenuCommand event]", interaction.commandName)
+      if (interaction.commandName === builder.name) return callback(interaction)
+    }
+  })
+}
+
 export async function getRegistrations() {
   const files = await glob("src/app/**/*.ts")
-  const imports = files.map((file) => import(`../app/${file.replace("src/app", "")}`))
+  const imports = files.map(
+    (file) => import(`../app/${file.replace("src/app", "")}`),
+  )
 
   // Let side-effects run
   await Promise.all(imports)
 
   return {
     handlers: handlersMap,
-    slashCommands: slashCommandsMap,
+    builders: buildersMap,
   }
 }
