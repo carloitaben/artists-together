54f2c932f95304fa66b64baff11d2d44dfa24ece
Carlo <hola.carlodominguez@gmail.com>
2023-09-24 13:16:47 +0200

    chore(bot-discord): improves live users logs readability

--- apps/bot-discord/src/app/live-users.ts
+++ apps/bot-discord/src/app/live-users.ts
@@ -1,9 +1,15 @@
-import { ActivityType } from "discord.js"
+import { ActivityType, Presence } from "discord.js"
 import { DiscordLiveUsers } from "db"
 
 import { registerEventHandler } from "~/lib/core"
 import { getGuild, getRole } from "~/lib/helpers"
 import { ROLES } from "~/lib/constants"
+
+function getStreamingActivity(presence: Presence | null) {
+  return presence?.activities.find(
+    (activity) => activity.type === ActivityType.Streaming,
+  )
+}
 
 registerEventHandler("ready", async (client) => {
   const guild = await getGuild(client)
@@ -13,9 +19,7 @@ registerEventHandler("ready", async (client) => {
 
     const hasArtistRole = member.roles.cache.has(ROLES.ARTIST)
     const hasLiveRole = member.roles.cache.has(ROLES.LIVE_NOW)
-    const streamingActivity = member.presence?.activities.find(
-      (activity) => activity.type === ActivityType.Streaming,
-    )
+    const streamingActivity = getStreamingActivity(member.presence)
 
     if (!hasArtistRole && hasLiveRole) {
       return Promise.all([
@@ -52,16 +56,28 @@ registerEventHandler("presenceUpdate", async (_, presence) => {
   const guild = await getGuild(presence.client)
   const role = await getRole(guild.roles, ROLES.LIVE_NOW)
   const hasArtistRole = presence.member.roles.cache.has(ROLES.ARTIST)
-  const streamingActivity = presence.activities.find(
-    (activity) => activity.type === ActivityType.Streaming,
-  )
+  const streamingActivity = getStreamingActivity(presence)
+
+  const oldPresence = getStreamingActivity(_)
 
   console.log(
     "[live-users] presenceUpdate for user",
     presence.member.user.username,
     {
-      oldPresence: _,
-      newPresence: presence,
+      oldPresence: {
+        applicationId: oldPresence?.applicationId,
+        name: oldPresence?.name,
+        url: oldPresence?.url,
+        details: oldPresence?.details,
+        state: oldPresence?.state,
+      },
+      newPresence: {
+        url: streamingActivity?.url,
+        name: streamingActivity?.name,
+        applicationId: streamingActivity?.applicationId,
+        details: streamingActivity?.details,
+        state: streamingActivity?.state,
+      },
     },
   )
 
