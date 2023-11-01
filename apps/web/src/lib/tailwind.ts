45716a533bc377e767e4a4419c38c443b009144e
Carlo <hola.carlodominguez@gmail.com>
2023-11-01 11:53:07 +0100

    feat(web): fuck next appdir

--- apps/web/src/lib/tailwind.ts
+++
@@ -1,8 +0,0 @@
-import resolveConfig from "tailwindcss/resolveConfig"
-import config from "../../tailwind.config"
-
-export const tailwind = resolveConfig(config)
-
-export type Screens = typeof tailwind.theme.screens
-
-export type Screen = keyof Screens
