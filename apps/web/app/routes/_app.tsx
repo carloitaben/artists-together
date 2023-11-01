5133a57fbfecfd25f9775fe4ed37b8f13659bb8e
Carlo <hola.carlodominguez@gmail.com>
2023-11-01 17:24:24 +0100

    feat(web): toast

--- apps/web/app/routes/_app.tsx
+++ apps/web/app/routes/_app.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "@remix-run/react"
 import Navigation from "~/components/Navigation"
+import Toast from "~/components/Toast"
 
 export default function Page() {
   return (
     <>
       <Outlet />
       <Navigation />
+      <Toast />
     </>
   )
 }
