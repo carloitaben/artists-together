8862509b125a43808e4ef6e73bda6a0c16dceb8a
Carlo <hola.carlodominguez@gmail.com>
2023-11-02 13:39:28 +0100

    fix(env): typed load function

--- packages/env/src/load.ts
+++ packages/env/src/load.ts
@@ -1,11 +1,22 @@
-import type { ZodTypeAny, TypeOf } from "node_modules/zod"
+import type { ZodTypeAny, TypeOf } from "zod"
 import { load as dotenvLoad } from "dotenv-mono"
 
-export function load<T extends ZodTypeAny[]>(...schemas: T) {
+type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends (
+  k: infer I
+) => void
+  ? I
+  : never
+
+type Merge<U> = UnionToIntersection<U> extends infer O
+  ? { [K in keyof O]: O[K] }
+  : never
+
+export function load<T extends ZodTypeAny[]>(
+  ...schemas: T
+): Merge<TypeOf<T[number]>> {
   const output = dotenvLoad()
 
-  // TODO: fix this any: Array to union -> iterate union with TypeOf<>
-  return schemas.reduce<any>(
+  return schemas.reduce<TypeOf<T[number]>>(
     (result, schema) => ({ ...result, ...schema.parse(output.env) }),
     {}
   )
