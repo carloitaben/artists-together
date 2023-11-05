47690112cd661cdf1ea2125dcfa383b6b03b1dcd
Carlo <hola.carlodominguez@gmail.com>
2023-11-05 16:32:39 +0100

    feat(web): padding prop to modal title

--- apps/web/app/components/Modal/Title.tsx
+++ apps/web/app/components/Modal/Title.tsx
@@ -1,22 +1,36 @@
-import { cx } from "cva"
+import type { VariantProps } from "cva"
+import { cva } from "cva"
 import * as Dialog from "@radix-ui/react-dialog"
 import type { ForwardedRef } from "react"
 import { forwardRef } from "react"
 
-type Props = Dialog.DialogTitleProps
+const title = cva({
+  base: "font-serif text-[2rem] leading-normal font-light text-gunpla-white-500",
+  variants: {
+    padding: {
+      true: "px-3",
+      false: "",
+    },
+  },
+  defaultVariants: {
+    padding: true,
+  },
+})
+
+type Props = Dialog.DialogTitleProps & VariantProps<typeof title>
 
 function Title(
-  { className, ...props }: Props,
+  { className, padding, ...props }: Props,
   ref: ForwardedRef<HTMLHeadingElement>,
 ) {
   return (
     <Dialog.Title
       {...props}
       ref={ref}
-      className={cx(
+      className={title({
         className,
-        "font-serif text-[2rem] leading-normal font-light text-gunpla-white-500",
-      )}
+        padding,
+      })}
     />
   )
 }
