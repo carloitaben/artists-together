eac58e3402d51a0500195155c95f99dd86631295
Carlo <hola.carlodominguez@gmail.com>
2023-11-07 12:40:34 +0100

    feat(web): avatar component

---
+++ apps/web/app/components/Avatar.tsx
@@ -0,0 +1,62 @@
+import * as AvatarPrimitive from "@radix-ui/react-avatar"
+import type { User } from "lucia"
+import type { ForwardedRef } from "react"
+import { forwardRef } from "react"
+import { cx } from "cva"
+import { tailwind } from "~/lib/tailwind"
+import Icon from "~/components/Icon"
+
+function getColor(theme: string) {
+  if (!(theme in tailwind.theme.colors)) {
+    throw Error(`Invalid color: ${theme}`)
+  }
+
+  const color =
+    tailwind.theme.colors[theme as keyof typeof tailwind.theme.colors]
+
+  if (typeof color === "string") {
+    throw Error(`Invalid color: ${theme}`)
+  }
+
+  return color
+}
+
+type Props = AvatarPrimitive.AvatarProps & {
+  className: string
+  user: Pick<User, "avatar" | "username" | "theme">
+}
+
+function Avatar(
+  { user, className, ...props }: Props,
+  ref: ForwardedRef<HTMLSpanElement>,
+) {
+  const color = getColor(user.theme)
+
+  return (
+    <AvatarPrimitive.Root
+      {...props}
+      ref={ref}
+      className={cx(className, "rounded-full inline-flex overflow-hidden")}
+    >
+      {user.avatar ? (
+        <AvatarPrimitive.Image
+          src={user.avatar}
+          alt={user.username}
+          draggable={false}
+          className="w-full h-full object-cover"
+          loading="lazy"
+          decoding="async"
+        />
+      ) : (
+        <AvatarPrimitive.AvatarFallback
+          className="w-full h-full p-1"
+          style={{ backgroundColor: color[50], color: color[700] }}
+        >
+          <Icon name="face" label={user.username} className="w-full h-full" />
+        </AvatarPrimitive.AvatarFallback>
+      )}
+    </AvatarPrimitive.Root>
+  )
+}
+
+export default forwardRef(Avatar)
