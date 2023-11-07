facdd74d782c5a1ec7c514bc0d14f5ee6a3e3eac
Carlo <hola.carlodominguez@gmail.com>
2023-11-07 18:17:38 +0100

    feat(web): context menu render prop

--- apps/web/app/components/ContextMenu/Content.tsx
+++ apps/web/app/components/ContextMenu/Content.tsx
@@ -1,13 +1,24 @@
 import * as ContextMenu from "@radix-ui/react-context-menu"
 import { cx } from "cva"
+import type { ForwardedRef } from "react"
+import { forwardRef } from "react"
+import { useContextMenuRootContext } from "./Root"
 
 type Props = ContextMenu.ContextMenuContentProps
 
-export default function Content({ className, ...props }: Props) {
+function Content(
+  { className, ...props }: Props,
+  ref: ForwardedRef<HTMLDivElement>,
+) {
+  const render = useContextMenuRootContext()
+
+  if (!render) return null
+
   return (
     <ContextMenu.Portal>
       <ContextMenu.Content
         {...props}
+        ref={ref}
         className={cx(
           className,
           "selection:bg-gunpla-white-300 selection:text-gunpla-white-900",
@@ -16,3 +27,5 @@ export default function Content({ className, ...props }: Props) {
     </ContextMenu.Portal>
   )
 }
+
+export default forwardRef(Content)
--- apps/web/app/components/ContextMenu/Trigger.tsx
+++ apps/web/app/components/ContextMenu/Trigger.tsx
@@ -1,1 +1,21 @@
-export { Trigger as default } from "@radix-ui/react-context-menu"
+import * as ContextMenu from "@radix-ui/react-context-menu"
+import type { ForwardedRef } from "react"
+import { forwardRef } from "react"
+import { useContextMenuRootContext } from "./Root"
+
+function Trigger(
+  { children, ...props }: ContextMenu.ContextMenuTriggerProps,
+  ref: ForwardedRef<HTMLSpanElement>,
+) {
+  const render = useContextMenuRootContext()
+
+  if (!render) return children
+
+  return (
+    <ContextMenu.Trigger {...props} ref={ref}>
+      {children}
+    </ContextMenu.Trigger>
+  )
+}
+
+export default forwardRef(Trigger)
