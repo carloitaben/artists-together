d7947e5e19e24fad4e0c35c8dca8a51ca429f193
Carlo <hola.carlodominguez@gmail.com>
2023-11-28 19:54:22 +0100

    chore: moves auth params logic to Auth component

--- apps/web/app/components/Auth/Auth.tsx
+++ apps/web/app/components/Auth/Auth.tsx
@@ -1,5 +1,8 @@
-import * as Modal from "~/components/Modal"
+import type { ReactNode } from "react"
+import { useEffect } from "react"
+import { useSearchParams } from "@remix-run/react"
 import { useUser } from "~/hooks/loaders"
+import * as Modal from "~/components/Modal"
 import AuthLoginForm from "./AuthLoginForm"
 import AuthUserProfile from "./AuthUserProfile"
 import AuthUserSocialMedia from "./AuthUserSocialMedia"
@@ -7,22 +10,44 @@ import AuthUserSettings from "./AuthUserSettings"
 import AuthUserSupport from "./AuthUserSupport"
 import AuthSignOutForm from "./AuthSignOutForm"
 
-export default function Auth() {
+type Props = {
+  children: ReactNode
+}
+
+export default function Auth({ children }: Props) {
+  const [params, setParams] = useSearchParams()
   const user = useUser()
 
+  useEffect(() => {
+    if (params.get("modal") !== "auth") return
+
+    setParams(
+      (params) => {
+        params.delete("modal")
+        return params
+      },
+      { replace: true },
+    )
+  }, [params, setParams])
+
+  const defaultOpen = params.get("modal") === "auth"
+
   return (
-    <Modal.Overlay>
-      {user ? (
-        <Modal.Content>
-          <AuthUserProfile />
-          <AuthUserSocialMedia />
-          <AuthUserSettings />
-          <AuthUserSupport />
-          <AuthSignOutForm />
-        </Modal.Content>
-      ) : (
-        <AuthLoginForm />
-      )}
-    </Modal.Overlay>
+    <Modal.Root defaultOpen={defaultOpen}>
+      {children}
+      <Modal.Overlay>
+        {user ? (
+          <Modal.Content>
+            <AuthUserProfile />
+            <AuthUserSocialMedia />
+            <AuthUserSettings />
+            <AuthUserSupport />
+            <AuthSignOutForm />
+          </Modal.Content>
+        ) : (
+          <AuthLoginForm />
+        )}
+      </Modal.Overlay>
+    </Modal.Root>
   )
 }
--- apps/web/app/components/Navigation/Navigation.tsx
+++ apps/web/app/components/Navigation/Navigation.tsx
@@ -1,32 +1,12 @@
-import { useSearchParams } from "@remix-run/react"
-import { useEffect } from "react"
-import Auth from "~/components/Auth"
-import * as Modal from "~/components/Modal"
+import Auth from "../Auth"
 import NavigationBottomAppBar from "./NavigationBottomAppBar"
 import NavigationRailAppBar from "./NavigationRailAppBar"
 
 export default function Navigation() {
-  const [params, setParams] = useSearchParams()
-
-  useEffect(() => {
-    if (params.get("modal") !== "auth") return
-
-    setParams(
-      (params) => {
-        params.delete("modal")
-        return params
-      },
-      { replace: true },
-    )
-  }, [params, setParams])
-
-  const defaultOpen = params.get("modal") === "auth"
-
   return (
-    <Modal.Root defaultOpen={defaultOpen}>
+    <Auth>
       <NavigationBottomAppBar />
       <NavigationRailAppBar />
-      <Auth />
-    </Modal.Root>
+    </Auth>
   )
 }
