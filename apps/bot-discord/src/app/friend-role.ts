701201180d0c632e2b56236e9e8ffa40327bc23b
Carlo <hello@carlo.works>
2024-08-26 09:07:26 +0200

    feat(bot-discord): add friend role after passing membership screening

--- apps/bot-discord/src/app/friend-role.ts
+++ apps/bot-discord/src/app/friend-role.ts
@@ -1,28 +1,14 @@
-import { env } from "~/lib/env"
 import { registerEventHandler } from "~/lib/core"
-import { getMember } from "~/lib/helpers"
 import { ROLES } from "~/lib/constants"
 
-const MESSAGE_ID = env.NODE_ENV === "development" ? "" : "1198612482823028849"
-
-const REACTION = "🔓"
-
-registerEventHandler("messageReactionAdd", async (partialReaction, partialUser) => {
-  if (!partialReaction.message.inGuild()) return
-  if (partialReaction.message.id !== MESSAGE_ID) return
-  if (partialUser.bot) return
-
-  if (partialReaction.emoji.name !== REACTION) {
-    return partialReaction.remove()
+registerEventHandler("guildMemberAdd", async (member) => {
+  if (!member.pending) {
+    await member.roles.add(ROLES.FRIEND, "Passed Membership Screening")
   }
+})
 
-  const member = await getMember(partialReaction.message.guild.members, partialUser.id)
-
-  if (member.roles.cache.has(ROLES.FRIEND)) return
-  if (member.roles.cache.has(ROLES.ARTIST)) return
-
-  await Promise.all([
-    member.roles.remove(ROLES.GUEST),
-    member.roles.add(ROLES.FRIEND),
-  ])
+registerEventHandler("guildMemberUpdate", async (oldMember, newMember) => {
+  if (oldMember.pending !== newMember.pending) {
+    await newMember.roles.add(ROLES.FRIEND, "Passed Membership Screening")
+  }
 })
