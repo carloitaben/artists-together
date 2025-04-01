358c2f5651d2a69a12df97e2d0c57c332d0f9458
Carlo <hola.carlodominguez@gmail.com>
2025-04-01 19:58:58 +0200

    fuck safari

--- apps/web/src/app/(about)/components/TextAnimation.tsx
+++ apps/web/src/app/(about)/components/TextAnimation.tsx
@@ -2,51 +2,98 @@
 
 import type { HTMLArkProps } from "@ark-ui/react/factory"
 import { ark } from "@ark-ui/react/factory"
-import { scroll, useAnimate, useInView } from "motion/react"
+import { scroll, useAnimate } from "motion/react"
 import type { ComponentRef, ForwardedRef } from "react"
 import { forwardRef, useEffect } from "react"
 import { mergeRefs } from "react-merge-refs"
 import SplitType from "split-type"
+import { onMeasure } from "~/lib/media"
 
 type Props = HTMLArkProps<"div">
 
 function TextAnimation(props: Props, ref: ForwardedRef<ComponentRef<"div">>) {
   const [scope, animate] = useAnimate<ComponentRef<"div">>()
-  const inView = useInView(scope)
 
   useEffect(() => {
-    if (!inView) return
+    function setup() {
+      const split = new SplitType(scope.current, {
+        split: "words",
+        wordClass: "blur-[--blur]",
+      })
 
-    const split = new SplitType(scope.current, {
-      split: "words",
-      wordClass: "blur-[--blur]",
+      if (!split.words) {
+        return split.revert
+      }
+
+      const cleanup = scroll(
+        animate(
+          split.words.map((word) => [
+            word,
+            {
+              opacity: [0, 1],
+              "--blur": ["4px", "0px"],
+            },
+          ]),
+        ),
+        {
+          target: scope.current,
+          offset: ["start center", "end end"],
+        },
+      )
+
+      return () => {
+        cleanup()
+        split.revert()
+      }
+    }
+
+    let width = 0
+    let cleanupAnimation: VoidFunction
+    const cleanupMeasure = onMeasure(scope.current, (rect) => {
+      if (width === rect.width) return
+      width = rect.width
+      cleanupAnimation = setup()
     })
 
-    if (!split.words) {
-      return split.revert()
-    }
-
-    const stop = scroll(
-      animate(
-        split.words.map((word) => [
-          word,
-          {
-            opacity: [0, 1],
-            "--blur": ["4px", "0px"],
-          },
-        ]),
-      ),
-      {
-        target: scope.current,
-        offset: ["start center", "end end"],
-      },
-    )
-
     return () => {
-      split.revert()
-      stop()
+      cleanupAnimation()
+      cleanupMeasure()
     }
-  }, [animate, inView, scope])
+  }, [animate, scope])
+
+  useEffect(() => {
+    // const split = new SplitType(scope.current, {
+    //   split: "words",
+    //   wordClass: "blur-[--blur]",
+    // })
+    // if (!split.words) {
+    //   return split.revert()
+    // }
+    // const stop = scroll(
+    //   animate(
+    //     split.words.map((word) => [
+    //       word,
+    //       {
+    //         opacity: [0, 1],
+    //         "--blur": ["4px", "0px"],
+    //       },
+    //     ]),
+    //   ),
+    //   {
+    //     target: scope.current,
+    //     offset: ["start center", "end end"],
+    //   },
+    // )
+    // const measure = onMeasure(scope.current, () => {
+    //   console.log("e")
+    //   split.split()
+    //   rect
+    // })
+    // return () => {
+    //   split.revert()
+    //   stop()
+    // }
+  }, [animate, scope])
 
   return <ark.div {...props} ref={mergeRefs([ref, scope])} />
 }
